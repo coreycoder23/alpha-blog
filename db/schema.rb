357ca69cc3bf130d007d17b7effa136e6dbfdbe1
Corey.Dev_ <corey.coder23@gmail.com>
2024-07-14 02:14:55 -0400

    add user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_12_154550) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_14_060150) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_12_154550) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
