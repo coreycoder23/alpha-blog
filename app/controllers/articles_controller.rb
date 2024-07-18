775c143d63839eea3dd87d904909a101932407ae
Corey.Dev_ <corey.coder23@gmail.com>
2024-07-18 11:25:54 -0400

    add pagination to user and article list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,8 @@ class ArticlesController < ApplicationController
 
   # GET /articles or /articles.json
   def index
-    @articles = Article.all
-      end
+    @articles = Article.paginate(page: params[:page], per_page: 3)
+  end
 
   # GET /articles/1 or /articles/1.json
   def show
