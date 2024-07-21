79bab08c16ded004f6ed7b89d7863e84ee1d8e4c
corey.coder23 <corey.coder23@gmail.com>
2024-07-21 08:02:59 -0400

    add admin users, update notice message color

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
